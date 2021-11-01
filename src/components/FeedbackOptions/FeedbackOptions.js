2f40ad17c0c50e356309ac8ec402e734cbcb6522
Anna Vasileva <anette.vasileva@gmail.com>
2021-11-01 21:33:05 +0300

    corrects keys

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -1,11 +1,16 @@
 import React from "react";
 import s from "./FeedbackOptions.module.css";
 import PropTypes from "prop-types";
+import { v4 as uuidv4 } from "uuid";
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return options.map((el) => {
     return (
-      <button onClick={() => onLeaveFeedback(el)} className={s.btn} key={el}>
+      <button
+        onClick={() => onLeaveFeedback(el)}
+        className={s.btn}
+        key={uuidv4()}
+      >
         {el}
       </button>
     );
